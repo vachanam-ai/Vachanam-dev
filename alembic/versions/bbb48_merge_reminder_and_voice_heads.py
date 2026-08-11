7061aa61654523a3d22c46e037108d6f1752e975
Vinay Rongala <vinayrongala.15@gmail.com>
2026-08-11 15:13:04 +0530

    fix(db): shorten reminder merge revision

--- alembic/versions/bbb48_merge_reminder_and_voice_heads.py
+++ alembic/versions/bbb48_merge_reminder_and_voice_heads.py
@@ -1,11 +1,11 @@
 """Merge reminder audit and clinic voice schema heads.
 
-Revision ID: bbb48_merge_reminder_and_voice_heads
+Revision ID: bbb48_merge_heads
 Revises: aaa47_reminder_dispatch_audit, ww46_branch_voice_clones
 """
 
 
-revision = "bbb48_merge_reminder_and_voice_heads"
+revision = "bbb48_merge_heads"
 down_revision = ("aaa47_reminder_dispatch_audit", "ww46_branch_voice_clones")
 branch_labels = None
 depends_on = None
